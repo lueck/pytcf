b06c0de55a474202afebb96f4cfa3619312004b8
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2019-01-26 03:10:14 +0100

    Added csv output

--- pytcf/token.py
+++ pytcf/token.py
@@ -1,3 +1,26 @@
+import csv
+
 class Token(object):
     """A token and further information on it like its ID assigned by the
     tokenizer, the lemma, the POS-tag etc. """
+
+    def getAttrsInOrder(self, order):
+        for attr in order:
+            yield getattr(self, attr, None)
+            
+
+def writeCsv(tokens,
+             filename,
+             attrs = ["tokenNum", "token", "tokenId", "start", "end", "srcStart", "srcEnd", "lemma", "POStag", "sentenceNum", "sentenceId", "numInSentence"],
+             sortAttr = lambda x: getattr(x, "tokenNum", None)):
+
+    def _strAttr(at):
+        if type(at) is unicode:
+            return at.encode("utf-8")
+        else:
+            return unicode(at).encode("utf-8")
+    
+    with open(filename, "wb") as csv_file:
+        wr = csv.writer(csv_file, delimiter = ",")
+        for tok in sorted(tokens.values(), key=sortAttr):
+            wr.writerow([_strAttr(at) for at in tok.getAttrsInOrder(attrs)])
--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,23 @@
 import os
 from unittest import TestCase
+import tempfile
 
 from pytcf.parser import parseTokens
+from pytcf.token import writeCsv
 
 dirname = os.path.dirname(__file__)
 rosenkranz = os.path.join(dirname, "rosenkranz_aesthetik_1853.tcf.xml")
 
 class TestParser(TestCase):
     def test_parseTokens(self):
-        t = parseTokens(rosenkranz)
+        t = parseTokens(rosenkranz) # test with DTA data
         self.assertEqual(len(t), 140455)
         self.assertEqual(t['w100'].token, u'H\xe4\xdflichen')
         self.assertEqual(t['w100'].lemma, u'H\xe4\xdfliche')
         self.assertEqual(t['w100'].POStag, "NN")
         self.assertEqual(t['w100'].sentenceNum, 18)
         self.assertEqual(t['w100'].numInSentence, 9)
+
+        rosenkranz_csv = tempfile.NamedTemporaryFile(mode="wb")
+        writeCsv(t, rosenkranz_csv.name)
+        
